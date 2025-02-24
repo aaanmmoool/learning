7fde68a8f6aaba91bfa3f46c03c53ac8d2f3137b
Anmol Singh <137929208+aaanmmoool@users.noreply.github.com>
2025-02-24 01:08:02 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,11 @@
+const name = "ANMOL"
+const repoCount = 20
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+
+// string methods 
+
+const n = "Anmol-hc";
+
+// indexOf , charAt , split , substring , replace
